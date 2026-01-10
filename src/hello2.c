51ca9bd69dd2c9bd312990a66cc463ac085280eb
KrishCoder <siriakshay08@gmail.com>
2026-01-10 15:52:38 -0500

    KrishChan

--- src/hello2.c
+++ src/hello2.c
@@ -1,5 +1,17 @@
-# include <stdio.h>
-int main(){
-    printf("Hello, World!\n");
+#include <stdio.h>
+#include <string.h>
+
+char currentCommand[1000];
+
+int main()
+{
+    for (int i = 0; i < 1000; i++)
+    {
+        scanf("%s", currentCommand);
+        // User can quit with "q"
+        if (strcmp("q", currentCommand) == 0)
+        { break; }
+        printf("The command you wrote is %s!\n", currentCommand);
+    }
     return 0;
 }
