582720c29d09085c9592de720cac7217374568c7
Benoit Thompson <ninjaben1107@gmail.com>
2026-01-10 15:38:10 -0500

    REPL
    Created a basic read-eval-print loop without evaluating anything.

--- src/cql.c
+++ src/cql.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
-char* cur_command;
+char currentCommand[1000];
 
 int main()
 {
     for (int i = 0; i < 1000; i++)
     {
-        scanf("%s", cur_command);
-        printf("The command you wrote is %s!", cur_command);
-
+        scanf("%s", currentCommand);
+        // User can quit with "q"
+        if (strcmp("q", currentCommand) == 0)
+        { break; }
+        printf("The command you wrote is %s!\n", currentCommand);
     }
     return 0;
 }
