ea17a7c882ea635bbc61614a78eb0279c778532a
Benoit Thompson <ninjaben1107@gmail.com>
2026-01-10 19:37:16 -0500

    deleting dsstore

---
+++ src/waratc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    while(1)
+    {
+        char* input;
+        scanf("%s", input);
+        if (strcmp(input, 'q') == 0) break;
+        
+    }
+    return 0;
+}
